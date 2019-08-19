f9e466179c7390f2a304353a47a6e0a970648835
Alexandre Sant Anna <programandodeaaz@gmail.com>
2019-08-19 20:07:29 -0300

    adição de slide

--- js/app.js
+++ js/app.js
@@ -60,3 +60,25 @@ $('.mobile-btn').click(function(){
 	$('.mobile-menu').toggleClass('active');
 });
 
+$('.slide > :first').addClass('active');
+
+
+function rotateSlide() {
+  var activeSlide = $('.slide > .active'),
+  nextSlide = activeSlide.next();
+
+  if(nextSlide.length == 0) {
+    nextSlide = $('.slide > :first');
+  }
+  activeSlide.removeClass('active');
+  nextSlide.addClass('active');
+}
+
+setInterval(rotateSlide, 2000);
+
+
+
+
+
+
+
